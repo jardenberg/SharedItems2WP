c410aa9b2330087066087de2629d47a55573a1db
Joakim Jardenberg <joakim@jardenberg.com>
2009-05-02 22:59:19 +0200

    Added old files

---
+++ shared-items-panel.php
@@ -0,0 +1,180 @@
+<?php if ($_POST['action'] == 'save') { ?><div id="message" class="updated fade" style="background-color: rgb(255, 251, 204);"><p><strong>Settings saved.</strong></p></div><?php } ?>
+
+<div class="wrap"><h2>Shared Items Post</h2>
+
+<div class="gdsr">
+<form method="post">
+	<?php wp_nonce_field('shared-1' ); ?>
+<input type="hidden" name="action" value="save" />
+<table class="form-table"><tbody>
+    <tr><th scope="row">Shared items url:</th>
+        <td>
+            <input type="text" name="share_url" id="adv_paypal_url" value="<?php echo $options["share_url"]; ?>" style="width: 720px" />
+        </td>
+    </tr>
+    <tr><th scope="row">Refresh:</th>
+        <td>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25">Period:</td>
+                    <td align="left">
+                    <select style="width: 180px;" name="refresh_period" id="refresh_period">
+                        <option value="monthly"<?php echo $options["refresh_period"] == 'monthly' ? ' selected="selected"' : ''; ?>>Monthly</option>
+                        <option value="weekly"<?php echo $options["refresh_period"] == 'weekly' ? ' selected="selected"' : ''; ?>>Weekly</option>
+                        <option value="daily"<?php echo $options["refresh_period"] == 'daily' ? ' selected="selected"' : ''; ?>>Daily</option>
+                    </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="150" height="25">Time:</td>
+                    <td align="left">
+                        <input maxlength="8" type="text" name="refresh_time" id="refresh_time" value="<?php echo $options["refresh_time"]; ?>" style="width: 170px" /> [format: HH:MM or HH:MM AP (AM/PM)]
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr><th scope="row">Post Template:</th>
+        <td>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25">Title:</td>
+                    <td><input type="text" name="post_title" id="post_title" value="<?php echo wp_specialchars(stripslashes($options["post_title"])); ?>" style="width: 570px" /><br />
+                    List of allowed template elements:</td>
+                </tr>
+            </table>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25"></td>
+                    <td width="130"><strong>%DATE%</strong></td>
+                    <td width="160"> : post publish date</td>
+                </tr>
+            </table>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25">Header:</td>
+                    <td><input type="text" name="post_header_template" id="post_header_template" value="<?php echo wp_specialchars($options["post_header_template"]); ?>" style="width: 570px" /></td>
+                </tr>
+                <tr>
+                    <td width="150" height="25">Footer:</td>
+                    <td><input type="text" name="post_footer_template" id="post_footer_template" value="<?php echo wp_specialchars($options["post_footer_template"]); ?>" style="width: 570px" /></td>
+                </tr>
+            </table>
+            <div class="gdsr-table-split"></div>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25">Item:</td>
+                    <td><input type="text" name="post_item_template" id="post_item_template" value="<?php echo wp_specialchars($options["post_item_template"]); ?>" style="width: 570px" /><br />
+                    List of allowed template elements:</td>
+                </tr>
+            </table>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25"></td>
+                    <td width="130"><strong>%TITLE%</strong></td>
+                    <td width="160"> : feed item title</td>
+                    <td width="10"></td>
+                    <td width="130"><strong>%DATE%</strong></td>
+                    <td width="160"> : item publish date</td>
+                </tr>
+                <tr>
+                    <td width="150" height="25"></td>
+                    <td width="130"><strong>%LINK%</strong></td>
+                    <td width="160"> : link for the feed item</td>
+                    <td width="10"></td>
+                    <td width="130"><strong>%NOTE%</strong></td>
+                    <td width="160"> : feed note</td>
+                </tr>
+            </table>
+            <div class="gdsr-table-split"></div>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25">Note:</td>
+                    <td><input type="text" name="post_note_template" id="post_note_template" value="<?php echo wp_specialchars($options["post_note_template"]); ?>" style="width: 570px" /><br />
+                    List of allowed template elements:</td>
+                </tr>
+            </table>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25"></td>
+                    <td width="130"><strong>%CONTENT%</strong></td>
+                    <td width="160"> : note content</td>
+                    <td width="10"></td>
+                    <td width="130"><strong>%AUTHOR%</strong></td>
+                    <td width="160"> : note author</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr><th scope="row">Post Settings:</th>
+        <td>
+            <table cellpadding="0" cellspacing="0" class="previewtable">
+                <tr>
+                    <td width="150" height="25">Author:</td>
+                    <td>
+                    <select name="post_author" id="post_author">
+                    <option selected="selected"
+                    <?php
+                        $all_users = get_users_of_blog();
+                        foreach ($all_users as $u) {
+                            $selected = "";
+                            if ($u->user_id == $options["post_author"]) $selected = ' selected="selected"';
+                            echo '<option value="'.$u->user_id.'"'.$selected.'>'.$u->display_name.'</option>';
+                        }
+                    ?>
+                    </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="150" height="25">Category:</td>
+                    <td>
+                    <?php 
+                        $dropdown_options = array('show_option_all' => '', 'hide_empty' => 0, 'hierarchical' => 1,
+                            'show_count' => 0, 'depth' => 0, 'orderby' => 'ID', 'selected' => $options["post_category"], 'name' => 'post_category');
+                        wp_dropdown_categories($dropdown_options);
+                    ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="150" height="25">Tags:</td>
+                    <td>
+                    <input type="text" name="post_tags" id="post_tags" value="<?php echo $options["post_tags"]; ?>" style="width: 570px" />
+                    </td>
+                </tr>
+                <tr>
+                    <td width="150" height="25">Comments:</td>
+                    <td>
+                    <input type="checkbox" name="post_comments" id="post_comments"<?php if ($options["post_comments"] == 1) echo " checked"; ?> /><label style="margin-left: 5px;" for="post_comments">Allow posting of comments</label>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</tbody></table>
+<p class="submit"><input class="inputbutton" type="submit" value="Save Options" name="saving"/></p>
+</form>
+</div>
+<div class="gdsr">
+<table class="form-table"><tbody>
+    <tr><th scope="row">Read feed:</th>
+        <td>
+            <form method="post">
+            	<?php wp_nonce_field('shared-2' ); ?>
+            <input type="hidden" name="action" value="runow" />
+                <input class="inputbutton" type="submit" value="Run Now" name="saving" />
+            </form>
+        </td>
+    </tr>
+    <tr><th scope="row">Reset settings:</th>
+        <td>
+            <form method="post">
+            	<?php wp_nonce_field('shared-3' ); ?>
+
+            <input type="hidden" name="action" value="reset" />
+                <input class="inputbutton" type="submit" value="Reset" name="saving" />
+            </form>
+        </td>
+    </tr>
+</table>
+</div>
+</div>
---
+++ shared-items-post.php
@@ -0,0 +1,311 @@
+<?php
+
+/*
+Plugin Name: Shared Items Post
+Plugin URI: http://www.googletutor.com/shared-items-post/
+Description: Scheduled automatic posting of Google Reader Shared Items.
+Version: 1.3.0
+Author:  Craig Fifield, Google Tutor
+Author URI: http://www.googletutor.com/ 
+*/
+
+require_once(ABSPATH.WPINC.'/class-snoopy.php');
+require_once(ABSPATH.WPINC.'/rss.php');
+require_once(dirname(__FILE__).'/simplepie.php');
+
+function gd_compare_items($a, $b) {
+    if ($a["entry_time"] == $b["entry_time"]) {
+        return 0;
+    }
+    return ($a["entry_time"] < $b["entry_time"]) ? -1 : 1;
+}
+
+if (!class_exists('SharedItemsPost')) {
+    class SharedItemsPost
+    {
+        var $log_file = "c:/log.txt";
+        
+        var $plugin_url;
+        var $plugin_path;
+        var $status = "";
+
+        var $o;
+
+        var $default_options = array(
+            'revision' => 11,
+            'share_url' => '',
+            'feed_url' => '',
+            'refresh_period' => 'weekly',
+            'refresh_time' => '06:00 AM',
+            'post_title' => 'Shared Items - %DATE%',
+            'post_header_template' => '&lt;ul&gt;',
+            'post_footer_template' => '&lt;/ul&gt;',
+            'post_item_template' => '&lt;li&gt;&lt;a href=&quot;%LINK%&quot;&gt;%TITLE%&lt;/a&gt;&lt;/li&gt;&lt;br&gt;%DATE% %NOTE%',
+            'post_note_template' => '- %CONTENT%',
+            'post_category' => 1,
+            'post_tags' => '',
+            'post_author' => 1,
+            'post_comments' => 1,
+            'last_crawl' => 0,
+            'last_refresh' => 0,
+            'last_refresh_feed' => 0
+        );
+        
+        function SharedItemsPost() {
+            $this->plugin_path_url();
+            $this->install_plugin();
+            $this->actions_filters();
+        }
+
+        function dump($msg, $object, $mode = "a+") {
+            $obj = print_r($object, true);
+            $f = fopen($this->log_file, $mode);
+            fwrite ($f, sprintf("[%s] : %s\r\n", current_time('mysql'), $msg));
+            fwrite ($f, "$obj");
+            fwrite ($f, "\r\n");
+            fclose($f);
+        }
+
+        function plugin_path_url() {
+            $this->plugin_url = WP_PLUGIN_URL.'/shared-items-post/';
+            $this->plugin_path = dirname(__FILE__).'/';
+            define('RSSSHAREITEMS_URL', $this->plugin_url);
+            define('RSSSHAREITEMS_PATH', $this->plugin_path);
+        }
+
+        function install_plugin() {
+            $this->o = get_option('shared-items-post-options');
+            
+            if (!is_array($this->o)) {
+                update_option('shared-items-post-options', $this->default_options);
+                $this->o = get_option('shared-items-post-options');
+            }
+            else {
+                foreach ($this->default_options as $key => $value)
+                    if (!isset($this->o[$key])) $this->o[$key] = $value;
+                $this->o["revision"] = $this->default_options["revision"];
+                update_option('shared-items-post-options', $this->o);
+            }
+        }
+
+        function actions_filters() {
+            add_action('init', array(&$this, 'init'));
+            add_action('admin_menu', array(&$this, 'admin_menu'));
+            add_action('admin_head', array(&$this, 'admin_head'));
+        }
+
+        function get_feed_url($share_url) {
+            $file = $this->get_file_contents($share_url);
+            preg_match('/xml".href="(.+?)"/', $file, $matches);
+            return $matches[1];
+        }
+        
+        function get_feed_contents($feed_url) {
+            $file = $this->get_file_contents($feed_url);
+            $rss = new MagpieRSS($file);
+            return $rss;
+        }
+        
+        function get_file_contents($url) {
+            $client = new Snoopy();
+            $client->agent = MAGPIE_USER_AGENT;
+            $client->read_timeout = MAGPIE_FETCH_TIME_OUT;
+            $client->use_gzip = MAGPIE_USE_GZIP;
+            $client->fetch($url);
+            $file = $client->results;
+            return $file;
+        }
+        
+        function init() {
+            if ($_POST['action'] == 'runow') {
+	            	check_admin_referer('shared-2');
+	            	
+                $this->generate_post();
+            }
+            else {
+                if ($_POST['action'] == 'reset') {
+                		check_admin_referer('shared-3');
+                		
+                    $this->o = $this->default_options;
+                    update_option("shared-items-post-options", $this->default_options);
+                }
+                
+                if ($_POST['action'] == 'save') {
+                		check_admin_referer('shared-1');
+                		
+                    $this->o["post_title"] = $_POST['post_title'];
+                    $this->o["post_tags"] = $_POST['post_tags'];
+                    $this->o["post_header_template"] = stripslashes(htmlentities($_POST['post_header_template'], ENT_QUOTES, 'UTF-8'));
+                    $this->o["post_footer_template"] = stripslashes(htmlentities($_POST['post_footer_template'], ENT_QUOTES, 'UTF-8'));
+                    $this->o["post_item_template"] = stripslashes(htmlentities($_POST['post_item_template'], ENT_QUOTES, 'UTF-8'));
+                    $this->o["post_note_template"] = stripslashes(htmlentities($_POST['post_note_template'], ENT_QUOTES, 'UTF-8'));
+                    $this->o["post_author"] = $_POST['post_author'];
+                    $this->o["post_category"] = $_POST['post_category'];
+                    $this->o["post_comments"] = isset($_POST['post_comments']) ? 1 : 0;
+                    $this->o["refresh_period"] = $_POST['refresh_period'];
+                    $this->o["refresh_time"] = $_POST['refresh_time'];
+                   
+
+                    
+                    update_option("shared-items-post-options", $this->o);
+
+                    $share_url = str_replace("https://", "http://", $_POST['share_url']);
+                    
+                    if ($share_url != $options["share_url"]) {
+                        $url = $this->get_feed_url($share_url);
+                        if ($url != "") {
+                            $this->o["share_url"] = $share_url;
+                            $this->o["feed_url"] = $url;
+                            update_option('shared-items-post-options', $this->o);
+                            $this->status = "ok";
+                        }
+                        else
+                            $this->status = "Feed URL not found.";
+                    }
+                }
+
+                if ($this->check_refresh()) 
+                    $this->generate_post();
+            }
+        }
+        
+        function generate_post() {
+        	 	if ($this->o["feed_url"] == "") return;
+        	 
+            $rss = new SimplePie();
+            $rss->set_feed_url($this->o["feed_url"]);
+            $rss->enable_cache(false);
+            $rss->enable_order_by_date(false);
+            $rss->init();
+            
+            $updated = $rss->get_channel_tags("http://www.w3.org/2005/Atom", "updated");
+            $last_update = strtotime(str_replace(array("T", "Z"), " ", $updated[0]["data"]));
+          
+            $new_items = array();
+            $post_content = "";
+            $newtime=0;
+            
+            if ($this->o["last_refresh_feed"] < $last_update) {
+                foreach ($rss->get_items() as $item) {
+                	                	                 
+ 	
+                    $entry_time = strtotime($item->get_local_date());
+                    $crawl_time = $item->data["attribs"]["http://www.google.com/schemas/reader/atom/"]["crawl-timestamp-msec"];
+ 							
+ 										if ($newtime==0)
+                	  	$newtime=$crawl_time;
+                	  
+                	  	
+                    if ($this->o["last_crawl"] < $crawl_time) {
+                        $new_item["crawl_time"] = $crawl_time;
+                        $new_item["entry_time"] = $entry_time;
+                        $new_item["title"] = $item->get_title();
+                        $new_item["link"] = $item->get_link();
+                        $annotation = $item->get_item_tags("http://www.google.com/schemas/reader/atom/", "annotation");
+                        if (isset($annotation)) {
+                            $note = html_entity_decode($this->o["post_note_template"]);
+                            $note = str_replace('%CONTENT%', $annotation[0]["child"]["http://www.w3.org/2005/Atom"]["content"][0]["data"], $note);
+                            $note = str_replace('%AUTHOR%', $annotation[0]["child"]["http://www.w3.org/2005/Atom"]["author"][0]["child"]["http://www.w3.org/2005/Atom"]["name"][0]["data"], $note);
+                        }
+                        else $note = "";
+                        $new_item["note"] = $note;
+                        
+                        $new_items[] = $new_item;
+                    }
+                }
+                
+                if (count($new_items) > 0) {
+                    foreach ($new_items as $item) {
+                        $item_date = date(get_option("date_format"), $item["entry_time"]);
+                        $import = html_entity_decode($this->o["post_item_template"]);
+                        $import = str_replace('%DATE%', $item_date, $import);
+                        $import = str_replace('%NOTE%', $item["note"], $import);
+                        $import = str_replace('%TITLE%', $item["title"], $import);
+                        $import = str_replace('%LINK%', $item["link"], $import);
+                        $post_content.= $import;
+                    }
+                }
+            }
+            
+            if ($post_content != "") {
+                $post_title = html_entity_decode($this->o["post_title"]);
+                $post_title = str_replace('%DATE%', date(get_option("date_format")), $post_title);
+                $post_header = html_entity_decode($this->o["post_header_template"]);
+                $post_footer = html_entity_decode($this->o["post_footer_template"]);
+                
+                $new_post = array();
+                
+                $new_post['comment_status'] = $this->o["post_comments"] == 1 ? 'open' : 'closed';
+                $new_post['post_author'] = $this->o["post_author"];
+                $new_post['post_content'] = $post_header.$post_content.$post_footer;
+                $new_post['post_status'] = 'publish';
+                $new_post['post_title'] = $post_title;
+                $new_post['post_type'] = 'post';
+                $new_post['post_category'] = array($this->o["post_category"]);
+                $new_post['tags_input'] = $this->o["post_tags"];
+
+                wp_insert_post($new_post);
+                $this->o["last_refresh_feed"] = $last_update;
+                $this->o["last_refresh"] = mktime();
+                $this->o["last_crawl"] =$newtime;
+                
+                update_option('shared-items-post-options', $this->o);
+            }
+        }
+        
+        function check_refresh() {
+            if ($this->o["feed_url"] != "") {
+                if ($this->o["last_refresh"] == 0) return true;
+                $pdate = $this->o["last_refresh"];
+                $timeparts = $this->convert_time($this->o["refresh_time"]);
+                switch ($this->o["refresh_period"]) {
+                    case "monthly":
+                        $next = mktime(0 + $timeparts[0], 0 + $timeparts[1], 0, date("m", $pdate) + 1, date("d", $pdate), date("Y", $pdate));
+                        break;
+                    case "weekly":
+                        $next = mktime(0 + $timeparts[0], 0 + $timeparts[1], 0, date("m", $pdate), date("d", $pdate) + 7, date("Y", $pdate));
+                        break;
+                    case "daily":
+                        $next = mktime(0 + $timeparts[0], 0 + $timeparts[1], 0, date("m", $pdate), date("d", $pdate) + 1, date("Y", $pdate));
+                        break;
+                }
+               
+                if (mktime() >= $next) return true;
+                else return false;
+            }
+            else return false;
+        }
+        
+        function convert_time($timer) {
+            $tp = split(" ", $timer);
+            if (count($tp) == 2) {
+                if ($tp[1] == "PM") {
+                    $tt = split(":", $tp[0]);
+                    $tt[0] = $tt[0] + 12;
+                    return $tt;
+                }
+                return split(":", $tp[0]);
+            }
+            else return split(":", $timer);
+        }
+
+        function admin_menu() {
+            add_submenu_page('options-general.php','Shared Items Post', 'Shared Items Post', 9, __FILE__, array($this, 'options_panel'));
+        }
+
+        function admin_head() {
+            echo('<link rel="stylesheet" href="'.$this->plugin_url.'shared-items-post.css" type="text/css" media="screen" />');
+        }
+        
+        function options_panel() {
+            $options = $this->o;
+            $status = $this->status;
+            
+            include($this->plugin_path.'shared-items-panel.php');
+        }
+    }
+    
+    $rssShare = new SharedItemsPost();
+}
+
+?>
